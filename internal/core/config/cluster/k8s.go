00683e226944fd9e26789698cc0e38f3a3a56c14
Ben Keith <bkeith@signalfx.com>
2019-08-28 11:37:44 -0400

    Agent Monitor Election
    Added new config option to monitors called `maxInstancesPerCluster` that
    uses a new generic election process across all agents in a cluster so
    that the monitor only runs so many times throughout the entire group of
    agents.
    
    This takes the original Kubernetes leader election code that was
    hardcoded into the kubernetes-cluster and kubernetes-events monitors and
    makes it generic and pluggable with other coordination mechanisms.  A
    proof-of-concept memcached implementation has also been added.
    Configuration for these pluggable coordinators is done via a new
    top-level config option `clustering`.
    
    More than one instance can be requested per cluster, but this requires
    that there be at least _n_ instances of the agent to function properly.
    This part is still a WIP.

---
+++ internal/core/config/cluster/k8s.go
@@ -0,0 +1,22 @@
+package cluster
+
+import (
+	"fmt"
+
+	"github.com/signalfx/signalfx-agent/internal/core/cluster"
+	"github.com/signalfx/signalfx-agent/internal/core/common/kubernetes"
+)
+
+type KubernetesConfig struct {
+	// Config for the K8s API client
+	KubernetesAPI *kubernetes.APIConfig `yaml:"kubernetesAPI" default:"{}"`
+}
+
+func (kc *KubernetesConfig) NewElector() (cluster.Elector, error) {
+	clientSet, err := kubernetes.MakeClient(kc.KubernetesAPI)
+	if err != nil {
+		return nil, fmt.Errorf("could not create Kubernetes client for leader election: %s", err)
+	}
+
+	return cluster.NewKubernetesElector(clientSet.CoreV1()), nil
+}
